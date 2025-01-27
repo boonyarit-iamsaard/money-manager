c5e91338a3f2acaa8e2790966d4d74ec1b10d4dc
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2025-01-27 18:49:44 +0700

    feat(admin): add user resource

---
+++ app/Filament/Admin/Resources/UserResource.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Filament\Admin\Resources;
+
+use App\Filament\Admin\Resources\UserResource\Pages\CreateUser;
+use App\Filament\Admin\Resources\UserResource\Pages\EditUser;
+use App\Filament\Admin\Resources\UserResource\Pages\ListUsers;
+use App\Models\User;
+use App\Role;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Support\Enums\IconPosition;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+// TODO: disable create action
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-square-3-stack-3d';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Grid::make([
+                    'default' => 1,
+                    'sm' => 2,
+                ])
+                    ->schema([
+                        TextInput::make('name')
+                            ->required()
+                            ->maxLength(255),
+                        TextInput::make('email')
+                            ->email()
+                            ->required()
+                            ->maxLength(255),
+                        Select::make('role')
+                            ->options(Role::class)
+                            ->native(false)
+                            ->required(),
+                        FileUpload::make('image')
+                            ->image()
+                            ->columnSpanFull(),
+                    ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                ImageColumn::make('image')
+                    ->label('')
+                    ->circular()
+                    ->defaultImageUrl(url('https://i.pravatar.cc/150')),
+                TextColumn::make('name')
+                    ->searchable(),
+                TextColumn::make('email')
+                    ->searchable()
+                    ->icon(fn (User $record): string => $record->email_verified_at ? 'heroicon-o-check-badge' : '')
+                    ->iconColor('success')
+                    ->iconPosition(IconPosition::After),
+                TextColumn::make('role')
+                    ->badge(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                EditAction::make(),
+            ])
+            ->bulkActions([
+                // Tables\Actions\BulkActionGroup::make([
+                //     Tables\Actions\DeleteBulkAction::make(),
+                // ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListUsers::route('/'),
+            'create' => CreateUser::route('/create'),
+            'edit' => EditUser::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Role.php
+++ app/Role.php
@@ -2,9 +2,11 @@
 
 namespace App;
 
+use Filament\Support\Contracts\HasColor;
+use Filament\Support\Contracts\HasLabel;
 use Illuminate\Support\Arr;
 
-enum Role: string
+enum Role: string implements HasColor, HasLabel
 {
     case ADMINISTRATOR = 'administrator';
     case USER = 'user';
@@ -15,5 +17,21 @@ enum Role: string
     public static function all(): array
     {
         return Arr::pluck(self::cases(), 'value');
+    }
+
+    public function getColor(): string
+    {
+        return match ($this) {
+            self::ADMINISTRATOR => 'danger',
+            self::USER => 'info',
+        };
+    }
+
+    public function getLabel(): string
+    {
+        return match ($this) {
+            self::ADMINISTRATOR => 'Administrator',
+            self::USER => 'User',
+        };
     }
 }
